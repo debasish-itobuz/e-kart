969154ead5c82b19adf1bc78e1c6f32c6d46d054
debasish-itobuz <debasish.das@itobuz.com>
2023-07-14 17:18:15 +0530

    2nd update

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const arrowLeft = document.querySelector('.left-arrow-button');
+const arrowRight = document.querySelector('.right-arrow-button');
+const imageSlider = document.querySelectorAll(".image-slider");
+
+let flag = 0;
+
+function sliderDisplay(index) {
+    for(let items of imageSlider) {
+        items.style.display = "none";
+    }
+
+    if(index >= imageSlider.length) {
+        flag = 0;
+        index = 0;
+    }else if(index < 0) {
+        flag = imageSlider.length - 1;
+        index = imageSlider.length - 1;
+    }
+    // imageSlider[index].style.display = "block";
+}
+
+sliderDisplay(flag);
+
+arrowLeft.addEventListener('click', ()=>{
+    let num = 1;
+    flag = flag -  num;
+    sliderDisplay(flag);
+})
+
+arrowRight.addEventListener('click', ()=>{
+    let num = 1;
+    flag = flag +  num;
+    sliderDisplay(flag);
+});
+
